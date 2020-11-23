ee17d6a45cf249bc0ddcbaa439f2591d4f773742
CroLinuxGamer <crolinuxgamer@gmail.com>
2020-11-23 16:37:36 +0100

    Added partitioning support.

--- setup-artix.sh
+++ setup-artix.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
+# this a an artix install script that helps mostly me to quickly
+# setup my artix linux installations. Since I dont want to do everything
+# manually every time I'm installing artix.
+
+# check boot mode
+printf "Checking your boot mode...\n\n"
+if [ -d "/sys/firmware/efi/efivars" ]; then
+    boot_mode="uefi" 
+else
+    boot_mode="bios"
+fi
+
 
 # Partitoning function that calls for users program of choice
 partitioning()
 {
     clear
-    printf "Listing your partitions\n\n"
+    printf "Listing your partitions\n\n" # listing partitions on the installation target
     lsblk
     printf "\n\n"
     read -p "Write path of the drive you will be using for partitioning: " drive
-    while true; do
+    while true; do # checking if the drive exists
         if [ -e "$drive" ]; then
             break
         else
@@ -18,7 +30,7 @@ partitioning()
             read -p "Write path of the drive you will be using for partitioning: " drive
         fi
     done
-    while true; do
+    while true; do # doing partitioning until you are satisfied
         printf "What program do you want to use for partitioning?\n1. fdisk\n2. cfdisk\n3. parted\n4. fdisk\n\n"
         read -p "Choose your preferred partitioning tool: " tool
         case $tool in
@@ -40,19 +52,23 @@ partitioning()
     done
 }
 
-# check boot mode
-echo "Checking your boot mode..."
-if [ -d "/sys/firmware/efi/efivars" ]; then
-    boot_mode="uefi" 
-else
-    boot_mode="bios"
-fi
-sleep 2
+formating()
+{
+    clear
+    while true; do
+        read -p "Write your root partition path " root
+       if [ -e "$root" ]; then
+           break
+       else
+           echo "That partition doesnt exist!"
+           echo "Please try again!"
+       fi
+    done
+}
 
-clear
-
+# checking if I already have partitions ready
 while true; do
-    read -p "Do you have ready partitions? y/n " yn
+    read -p "Do you have ready partitions? " yn
     case $yn in
         [Yy]* ) break ;;
         [Nn]* ) partitioning; break ;;
@@ -62,4 +78,15 @@ done
 
 clear
 
-echo "You arch install is finished you can now reboot your pc!"
+# checking if there is any need for formating partitionsa
+# and making swap partitions if present
+while true; do
+    read -p "Do you need to format any partitions? " yn
+    case $yn in
+        [Yy]* ) break ;;
+        [Nn]* ) formating; break ;;
+        * ) echo "Please answer with Y or N" ;;
+    esac
+done
+
+echo "Your arch install is finished you can now reboot your pc!"
