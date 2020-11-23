b36a9c742b1f578f60257e2f623a4d34f619c2b0
CroLinuxGamer <crolinuxgamer@gmail.com>
2020-11-23 21:39:31 +0100

    Added system clock settings.

--- setup-artix.sh
+++ setup-artix.sh
@@ -337,6 +337,7 @@ artools-chroot /mnt rm -f chroot-arch
 artools-chroot /mnt curl -O https://raw.githubusercontent.com/CroLinuxGamer/artix-auto/main/chroot-artix.sh
 artools-chroot /mnt chmod +x chroot-artix.sh
 artools-chroot /mnt ./chroot-artix.sh
+artools-chroot /mnt rm -f chroot-arch
 
 umount -R /mnt
 
