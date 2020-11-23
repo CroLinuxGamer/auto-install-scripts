a9e6e38fb5ce8398889c9eef61b5dcc36f6c0c90
CroLinuxGamer <crolinuxgamer@gmail.com>
2020-11-23 20:43:42 +0100

    Added system clock settings.

--- chroot-artix.sh
+++ chroot-artix.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# check boot mode
+printf "Checking your boot mode...\n\n"
+if [ -d "/sys/firmware/efi/efivars" ]; then
+    boot_mode="uefi" 
+else
+    boot_mode="bios"
+fi
+
 system_clock()
 {
     ls /usr/share/zoneinfo
@@ -13,6 +21,44 @@ system_clock()
             echo "Please enter a valid zone"
         fi
     done         
+}
+
+grub_install()
+{
+    clear
+    echo "Installing grub..."
+    if [ $boot_mode = "bios" ]; then
+        pacman -S grub
+    else
+        pacman -S grub efibootmgr
+    fi
+    while true; do
+        read -p "Are you dualbooting with more operating systems?" yn
+        case $yn in
+            [Yy]* ) pacman -S os-prober; break ;;
+            [Nn]* ) break ;;
+            * ) echo "Please answer with Y or N" ;;
+        esac
+    done
+    echo "listing your partitions"
+    lsblk
+    while true; do
+        read -p "Write the path to your boot partitiom " boot
+        if [ -e "$boot" ]; then
+            if [ $boot_mode = "bios"]; then
+                grub-install --recheck $boot
+                break
+            else
+                grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=artix
+                break
+            fi
+        else
+            echo "That partitiom doesn't exist!"
+            echo "Please try again!"
+        fi
+    done
+    echo "Generating grub config..."
+    grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 # settings up system clock
@@ -52,3 +98,6 @@ clear
 echo "Now you will set tty locale if you changed it in the artix install"
 sleep 5
 nano /etc/vconsole.conf
+
+# grub installation
+grub_install
