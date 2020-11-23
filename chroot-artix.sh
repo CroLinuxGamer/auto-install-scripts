5744fa55e53e9f6e1f8e28806df02690f58b0f6d
CroLinuxGamer <crolinuxgamer@gmail.com>
2020-11-23 19:44:41 +0100

    Added system clock settings.

--- chroot-artix.sh
+++ chroot-artix.sh
@@ -1,1 +1,20 @@
 #!/bin/sh
+
+system_clock()
+{
+    ls /usr/share/zoneinfo
+    while true; do
+        read -p "Chose your zone: " zone
+        if [ -d "/usr/share/zoneinfo/$zone" ]; then
+            ln -sf /usr/share/zoneinfo/$zone /etc/localtime
+            break
+        else 
+            echo "That zone doesn't exist!"
+            echo "Please enter a valid zone"
+        fi
+    done         
+}
+
+# settings up system clock
+echo "Setting up system clock..."
+system_clock
