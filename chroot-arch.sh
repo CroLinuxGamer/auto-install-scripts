d7504eaace589f1d9a0a8b7e2bc788a1762c20d5
CroLinuxGamer <crolinuxgamer@gmail.com>
2020-12-17 10:29:22 +0100

    Created new arch install scripts.

---
+++ chroot-arch.sh
@@ -0,0 +1,216 @@
+#!/bin/sh
+
+# check boot mode
+printf "Checking your boot mode...\n\n"
+if [ -d "/sys/firmware/efi/efivars" ]; then
+    boot_mode="uefi" 
+else
+    boot_mode="bios"
+fi
+
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+
+system_clock()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[System Clock]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    ls /usr/share/zoneinfo
+    while true; do
+        read -p "Chose your zone: " zone
+        if [ -f "/usr/share/zoneinfo/$zone" ]; then
+            ln -sf /usr/share/zoneinfo/"$zone" /etc/localtime
+            break
+        else 
+            echo "That zone doesn't exist!"
+            echo "Please enter a valid zone"
+        fi
+    done         
+}
+
+grub_install()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Grub install]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    echo "Installing grub..."
+    if [ $boot_mode = "bios" ]; then
+        pacman -S grub -y
+    else
+        pacman -S grub efibootmgr -y
+    fi
+    while true; do
+        read -p "Are you dualbooting with more operating systems?" yn
+        case $yn in
+            [Yy]* ) pacman -S os-prober; break ;;
+            [Nn]* ) break ;;
+            * ) echo "Please answer with Y or N" ;;
+        esac
+    done
+    echo "listing your partitions"
+    lsblk
+    while true; do
+        read -p "Write the path to your disk " disk
+        if [ -e "$disk" ]; then
+            if [ $boot_mode = "bios" ]; then
+                grub-install --recheck $disk
+                break
+            else
+                grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=artix
+                break
+            fi
+        else
+            echo "That partitiom doesn't exist!"
+            echo "Please try again!"
+        fi
+    done
+    echo "Generating grub config..."
+    grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+network_manager()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Network Manager]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "Choose your init system\n1. openrc\n2. runit\n3. s6\n"
+    while true; do
+        read -p "Type the number of the desired init system: " init
+        case $init in
+            [1]* ) pacman -S networkmanager-openrc -y ;  rc-update add NetworkManager ; break ;;
+            [2]* ) pacman -S networkmanager-runit -y ; printf "After rebooting you need to run \nln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/defaul\nto be able to use metwork manager" ; sleep 5;break ;;
+            [2]* ) pacman -S networkmanager-s6 -y ; printf "After rebooting you need to run\ns6-rc-bundle -c /etc/s6/rc/compiled add default NetworkManager\n to be able to use network manager"; sleep 5;break ;;
+            * ) echo "Please answer with the nubmer before the name of the init system!" ;;
+        esac
+    done
+}
+
+# settings up system clock
+clear
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo "Setting up system clock..."
+system_clock
+
+# setting hardware clock
+clear
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo "Setting up hardware clock..."
+hwclock --systohc 
+sleep 2
+
+# installing nano
+clear
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo "Installing nano..."
+pacman -S nano
+
+# locale settings
+clear 
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo "Now you will need to uncomment the locale's you desire in the next file"
+sleep 5
+nano /etc/locale.gen
+
+# generating locale's
+clear
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+locale-gen
+
+# setting languange in locale.conf
+clear
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo "Now you need to set the locale variable in locale.conf"
+sleep 5
+nano /etc/locale.conf
+
+# setting up tty locale
+clear
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo "Now you will set tty locale if you changed it in the artix install"
+sleep 5
+nano /etc/conf.d/keymaps
+
+# grub installation
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+grub_install
+
+# settins root password
+clear
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo "Setting root password"
+passwd
+
+# setting user password
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+read -p "Type in your user name: " user
+useradd -m $user
+passwd $user
+
+# adding user to basic groups
+usermod -aG games $user
+usermod -aG wheel $user
+usermod -aG tty $user
+usermod -aG audio $user
+usermod -aG input $user
+usermod -aG video $user
+
+# set your hostname
+clear
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo "Set your hostname in the next file"
+sleep 5
+nano /etc/hostname
+
+# set your hosts
+clear
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo "Set your hosts in the next file"
+sleep 5
+nano /etc/hosts
+
+# enable wheel group to use sudo
+clear 
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo "enable wheel group in the next file to be able to use sudo"
+sleep 5
+env EDITOR=nano visudo
+clear
+
+# installing network manager
+clear 
+COLUMNS=$(tput cols)
+title="[Chroot Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+network_manager
+
---
+++ setup-arch.sh
@@ -0,0 +1,436 @@
+#!/bin/sh
+
+# this a an artix install script that helps mostly me to quickly
+# setup my artix linux installations. Since I dont want to do everything
+# manually every time I'm installing artix.
+
+# nice clean screen because words are bloat ;)
+
+clear
+
+COLUMNS=$(tput cols)
+title="[Auto Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+
+# check boot mode
+printf "Checking your boot mode...\n\n"
+if [ -d "/sys/firmware/efi/efivars" ]; then
+    boot_mode="uefi" 
+else
+    boot_mode="bios"
+fi
+sleep 2
+
+
+# Partitoning function that calls for users program of choice
+partitioning()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Partitioning]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "Listing your partitions...\n\n" # listing partitions on the installation target
+    lsblk
+    printf "\n\n"
+    read -p "Write path of the drive you will be using for partitioning: " drive
+    while true; do # checking if the drive exists
+        clear
+        if [ -e "$drive" ]; then # if drive exists
+            break # continue
+        else
+            echo "That path is wrong?"
+            echo "Try again!"
+            read -p "Write path of the drive you will be using for partitioning: " drive
+        fi
+    done
+    while true; do # doing partitioning until you are satisfied
+        printf "What tool do you want to use for partitioning?\n1. fdisk\n2. cfdisk\n3. parted\n4. fdisk\n\n" # listing partitioning tools
+        read -p "Choose your preferred partitioning tool: " tool
+        case $tool in
+            [1]* ) fdisk $drive ;;
+            [2]* ) cfdisk $drive ;;
+            [3]* ) parted $drive ;;
+            [4]* ) fdisk $drive ;;
+        esac
+        clear 
+        printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+        printf "Listing your newly created partitions\n\n"
+        lsblk
+        printf "\n\n"
+        read -p "Are you done with partitioning? " yn
+        case $yn in
+            [Yy]* ) break ;;
+            [Nn]* ) ;;
+            * ) echo "Please answer with Y or N" ;;
+        esac
+    done
+}
+
+# formating the home partition
+home_format()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Formating]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    lsblk
+    printf "\n\nFormating your home partition...\n\n"
+    while true; do
+        read -p "Write the path to your home partitiom: " home
+        if [ -e "$home" ]; then
+            mkfs.ext4 -L HOME $home
+            break
+        else
+            echo "That partitiom doesn't exist!"
+            echo "Please try again!"
+        fi
+    done
+}
+
+# formating the 
+boot_format()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Formating]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    lsblk
+    printf "\n\nFormating your boot partitiom...\n\n"
+    while true; do
+        read -p "Write the path to your boot partitiom: " boot
+        if [ -e "$boot" ]; then
+            if [ $boot_mode = "bios" ]; then # checking the boot mode
+                mkfs.ext4 -L BOOT $boot
+                break
+            else 
+                 mkfs.fat -F 32 $boot
+                 break
+            fi
+        else
+            echo "That partitiom doesn't exist!"
+            echo "Please try again!"
+        fi
+    done
+}
+
+# making swap format
+swap_format()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Formating]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    lsblk
+    printf "\n\nFormating your swap partition...\n\n"
+    while true; do
+        read -p "Write the path to swap partitiom: " swap
+        if [ -e "$swap" ]; then
+            mkswap -L SWAP $swap
+            break
+        else
+            echo "That partitiom doesn't exist!"
+            echo "Please try again!"
+        fi
+    done
+}
+
+# the main formating function
+formating()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Formating]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "Listing partitions...\n\n"
+    lsblk
+    read -p "Write your root partition path: " root
+    while true; do
+        if [ -e "$root" ]; then
+            mkfs.ext4 -L ROOT $root
+            break
+        else
+            echo "That partition doesnt exist!"
+            echo "Please try again!"
+        read -p "Write your root partition path: " root
+        fi
+    done
+    while true; do
+        read -p "Do you have a separate home partition? " yn
+        case $yn in
+            [Yy]* ) home_format; break ;;
+            [Nn]* ) break ;;
+            * ) echo "Please answer with Y or N" ;;
+        esac
+    done
+    while true; do
+        read -p "Do you have a separate boot partition? " yn
+        case $yn in
+            [Yy]* ) boot_format; break ;;
+            [Nn]* ) break ;;
+            * ) echo "Please answer with Y or N" ;;
+        esac
+    done
+    while true; do
+        read -p "Do you have a swap partition? " yn
+        case $yn in
+            [Yy]* ) swap_format;break ;;
+            [Nn]* ) break ;;
+            * ) echo "Please answer with Y or N" ;;
+        esac
+    done
+}
+
+mount_home()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Mounting]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "Listing your partitions..."
+    lsblk
+    while true; do
+        read -p "Write the path to your home partition: " home
+        if [ -e "$home" ]; then
+            echo "Mounting home partition..."
+            mkdir /mnt/home
+            mount $home /mnt/home
+            sleep 2
+            break
+        else
+            echo "That partitiom doesn't exist!"
+            echo "Please try again!"
+        fi
+    done
+}
+
+mount_boot()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Mounting]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "Listing your partitions..."
+    lsblk
+    while true; do
+        read -p "Write the path to your boot partition: " boot
+        if [ -e "$boot" ]; then
+            echo "Mounting boot partition..."
+            mkdir /mnt/boot
+            mount $boot /mnt/boot
+            sleep 2
+            break
+        else
+            echo "That partitiom doesn't exist!"
+            echo "Please try again!"
+        fi
+    done
+}
+
+mount_swap()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Mounting]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "Listing your partitions..."
+    lsblk
+    while true; do
+        read -p "Write the path to your swap partition: " swap
+        if [ -e "$swap" ]; then
+            echo "Mounting swap partition..."
+            swapon $swap
+            sleep 2
+            break
+        else
+            echo "That partitiom doesn't exist!"
+            echo "Please try again!"
+        fi
+    done
+}
+
+mounting()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Mounting]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "Listing your partitions..."
+    lsblk
+    while true; do
+        read -p "Write the path to your root partition: " root
+        if [ -e "$root" ]; then
+            echo "Mounting root partition..."
+            sleep 2
+            mount $root /mnt
+            break
+        else
+            echo "That partitiom doesn't exist!"
+            echo "Please try again!"
+        fi
+    done
+    while true; do
+        read -p "Do you have a home partition? " yn
+        case $yn in
+            [Yy]* ) mount_home;break ;;
+            [Nn]* ) break ;;
+            * ) echo "Please answer with Y or N" ;;
+        esac
+    done
+        while true; do
+        read -p "Do you have a boot partition? " yn
+        case $yn in
+            [Yy]* ) mount_boot;break ;;
+            [Nn]* ) break ;;
+            * ) echo "Please answer with Y or N" ;;
+        esac
+    done
+        while true; do
+        read -p "Do you have a swap partition? " yn
+        case $yn in
+            [Yy]* ) mount_swap;break ;;
+            [Nn]* ) break ;;
+            * ) echo "Please answer with Y or N" ;;
+        esac
+    done
+
+}
+
+base_install()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Base installation]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "Choose your init system\n1. openrc\n2. runit\n3. s6\n"
+    while true; do
+        read -p "Type the number of the desired init system: " init
+        case $init in
+            [1]* ) basestrap /mnt base base-devel openrc; break ;;
+            [2]* ) basestrap /mnt base base-devel runit elogind-runit; break ;;
+            [2]* ) basestrap /mnt base base-devel s6 elogind-s6; break ;;
+            * ) echo "Please answer with the nubmer before the name of the init system!" ;;
+        esac
+    done
+}
+
+kernel_install()
+{
+    clear
+    COLUMNS=$(tput cols)
+    title="[Kernel installation]"
+    printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "Choose your desired kernel\n1. linux\n2. linux-lts\n3. linux-zen\n"
+    while true; do
+        read -p "Type the nubmer of your desired kernel: " kernel
+        case $kernel in
+            [1]* ) basestrap /mnt linux linux-firmware; break ;;
+            [2]* ) basestrap /mnt linux-lts linux-firmware; break ;;
+            [3]* ) basestrap /mnt linux-zen linux-firmware; break ;;
+            * ) echo "Please answer with the number before the name of the kernel!"
+        esac
+    done
+}
+unmount_swap()
+{
+    while true; do
+        read -p "Write the path to your swap partition: " swap
+        if [ -e "$swap" ]; then
+            echo "Turning off swap ..."
+            swapoff $swap
+            sleep 2
+            break
+        else
+            echo "That partitiom doesn't exist!"
+            echo "Please try again!"
+        fi
+    done
+}
+
+# checking if I already have partitions ready
+while true; do
+    read -p "Do you have ready partitions? " yn
+    case $yn in
+        [Yy]* ) break ;;
+        [Nn]* ) partitioning; break ;;
+        * ) echo "Please answer with Y or N" ;;
+    esac
+done
+
+# checking if there is any need for formating partitionsa
+# and making swap partitions if present
+clear
+COLUMNS=$(tput cols)
+title="[Auto Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+while true; do
+    read -p "Do you need to format any partitions? " yn
+    case $yn in
+        [Yy]* ) formating; break ;;
+        [Nn]* ) break ;;
+        * ) echo "Please answer with Y or N" ;;
+    esac
+done
+
+# mounting partitions
+clear
+COLUMNS=$(tput cols)
+title="[Auto Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+printf "Mounting partitions...\n"
+sleep 2
+mounting
+
+# installing base system
+clear
+COLUMNS=$(tput cols)
+title="[Auto Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+printf "Installing base system...\n"
+sleep 2
+base_install
+
+# installing kernel
+clear
+COLUMNS=$(tput cols)
+title="[Auto Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+printf "Installing kernel...\n"
+sleep 2
+kernel_install
+
+# generating fstab
+clear
+COLUMNS=$(tput cols)
+title="[Auto Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+printf "Generating fstab...\n"
+fstabgen -U /mnt >> /mnt/etc/fstab
+sleep 2
+
+clear
+COLUMNS=$(tput cols)
+title="[Auto Artix]"
+printf "%*s\n\n" $(((${#title}+$COLUMNS)/2)) "$title"
+printf "Downloading the chroot part of the script..."
+artools-chroot /mnt curl -sO https://raw.githubusercontent.com/CroLinuxGamer/auto-install-scripts/main/chroot-artix.sh
+artools-chroot /mnt chmod +x chroot-artix.sh
+sleep 2
+artools-chroot /mnt ./chroot-artix.sh
+artools-chroot /mnt rm -f chroot-arch.sh
+
+# unmount drives and turning off swap
+print "Unmounting drives..."
+umount -R /mnt
+lsblk
+while true; do
+    read -p "Did you enable swap? " yn
+    case $yn in
+        [Yy]* ) unmount_swap; break ;;
+        [Nn]* ) break ;;
+        * ) echo "Please answer with Y or N" ;;
+    esac
+done
+
+clear
+
+echo "Your artix install is finished you can now reboot your pc!"
